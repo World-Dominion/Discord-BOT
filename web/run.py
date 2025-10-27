4f03831341565b350707d81ee4c796d54e92fd65
Amine <IT-Amine@users.noreply.github.com>
2025-10-27 17:16:04 +0100

    New app.py update

--- web/run.py
+++ web/run.py
@@ -59,4 +59,9 @@ print("⚠️  Pour arrêter le serveur, utilisez Ctrl+C")
 print()
 
 # Importer et démarrer l'application
+from app import socketio, app
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, debug=False, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
 
